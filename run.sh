45d99a7ef0bdaca6f765b1d10fe3d62dd88fcd6d
Vernon Jefferson <vernon@kivera.io>
2023-07-29 13:39:08 +1000

    Updates

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-export KIVERA_CREDENTIALS=$1
-
-echo $KIVERA_CREDENTIALS
+export KIVERA_CREDENTIALS="$1"
 
 docker run -t --rm \
   -p 8080:8080 -p 8090:8090 \
   --ulimit nofile=90000:90000 \
-  -e KIVERA_CREDENTIALS=${KIVERA_CREDENTIALS} \
+  -e KIVERA_CREDENTIALS="${KIVERA_CREDENTIALS}" \
   public.ecr.aws/n8p1g0n4/kivera-proxy:latest
